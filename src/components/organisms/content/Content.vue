de56c4550c00bbd39c76ff172a2460b74f034aa4
umar <umar.dev500@gmail.com>
2022-09-25 22:15:15 +0700

    add searchbar

--- src/components/organisms/content/Content.vue
+++ src/components/organisms/content/Content.vue
@@ -2,6 +2,7 @@
   <div class="ps-lg-4 mt-5 mt-lg-0">
     <CollectionList />
     <ShopAllList />
+    <Search />
   </div>
 </template>
 
@@ -9,11 +10,13 @@
 import { Options, Vue } from "vue-class-component";
 import CollectionList from "./CollectionList.vue";
 import ShopAllList from "./ShopAllList.vue";
+import Search from "./Search.vue";
 
 @Options({
   components: {
     CollectionList,
     ShopAllList,
+    Search,
   },
 })
 export default class Content extends Vue {}
---
+++ src/components/organisms/content/Search.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="px-lg-5 mt-4">
+    <input
+      type="text"
+      placeholder="Search"
+      class="form-control rounded-pill search-input px-lg-3"
+    />
+  </div>
+</template>
+
+<style lang="scss">
+.search-input {
+  background-color: #e7e7e7 !important;
+  font-size: 18px !important;
+}
+
+.search-input:focus {
+  background-color: white !important;
+}
+</style>
