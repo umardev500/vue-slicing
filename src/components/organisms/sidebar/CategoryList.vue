9d5895cec1aecf4fd21e759d60074ea34b15066d
umar <umar.dev500@gmail.com>
2022-09-25 19:38:06 +0700

    add show all button

--- src/components/organisms/sidebar/CategoryList.vue
+++ src/components/organisms/sidebar/CategoryList.vue
@@ -4,6 +4,10 @@
     <CategoryListing value="All (1080)" />
     <CategoryListing value="Arts & Crafts (38)" />
     <CategoryListing value="Fitness & Sports (469)" />
+    <div class="ms-3">
+      <span class="show-all-text fw-bold">Show all</span>
+      <i class="fa-solid fa-angle-down ms-2 show-all-caret"></i>
+    </div>
   </div>
 </template>
 
@@ -20,3 +24,14 @@ import Label from "@/components/atoms/label/Label.vue";
 })
 export default class CategoryList extends Vue {}
 </script>
+
+<style lang="scss">
+.show-all-text {
+  color: $appColorPrimary;
+  opacity: 0.8;
+}
+
+.show-all-caret {
+  color: $appColorPrimary;
+}
+</style>
